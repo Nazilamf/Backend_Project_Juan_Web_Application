b181c5d7b46222ee428c9b390fe143f41b337d67
Nazilamf <nazilamf@code.edu.az>
2023-07-13 17:40:53 +0400

    Basket

--- BackendProject/Backend-Project_Juan_Web_Application/Controllers/ProductController.cs
+++ BackendProject/Backend-Project_Juan_Web_Application/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using Backend_Project_Juan_Web_Application.DAL;
 using Backend_Project_Juan_Web_Application.Entities;
+using Backend_Project_Juan_Web_Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.Packaging.Signing;
 
 namespace Backend_Project_Juan_Web_Application.Controllers
 {
@@ -19,5 +22,47 @@ namespace Backend_Project_Juan_Web_Application.Controllers
             Product product = _context.Products.Include(x=>x.ProductImages).Include(x=>x.ProductSizes).Include(x=>x.Category).Include(x=>x.Brand).FirstOrDefault(x => x.Id== id);
             return PartialView("_ProductModalPartial", product);
         }   
+
+        public IActionResult AddToBasket(int id)
+        {
+            var basketstr = Request.Cookies["basket"];
+            List<BasketCookieItemViewModel> items = null;
+            if (basketstr== null)
+            {
+
+                 items = new List<BasketCookieItemViewModel>();
+            }
+            else
+            {
+                items= JsonConvert.DeserializeObject<List<BasketCookieItemViewModel>>(basketstr);
+               
+            }
+
+            BasketCookieItemViewModel item = items.FirstOrDefault(x=>x.ProductId== id); 
+            if(item==null)
+            {
+                item = new BasketCookieItemViewModel()
+                {
+                    ProductId= id,
+                    Count=1
+                };
+                items.Add(item);
+            }
+            else
+            {
+                item.Count++;
+            }
+         
+            items.Add(item);
+            HttpContext.Response.Cookies.Append("basket", JsonConvert.SerializeObject(items));
+            return RedirectToAction("Index","home");   
+        }
+
+        public IActionResult ShowBasket()
+        {
+            var datastr = HttpContext.Request.Cookies["basket"];
+            var data = JsonConvert.DeserializeObject<List<BasketCookieItemViewModel>>(datastr);
+            return Json(data);
+        }
     }
 }
---
+++ BackendProject/Backend-Project_Juan_Web_Application/ViewModels/BasketCookieItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Project_Juan_Web_Application.ViewModels
+{
+    public class BasketCookieItemViewModel
+    {
+        public int ProductId { get; set; }
+        public int Count { get; set; }  
+    }
+}
